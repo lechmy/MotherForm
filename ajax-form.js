b3b4511a1889d37ede060b9b646875f01d0edc0e
Aleksandar <aca@software-advantage.com>
2021-02-04 23:56:07 +0100

    #Cleanup

--- ajax-form.js
+++ ajax-form.js
@@ -578,37 +578,3 @@ function AjaxForm(config) {
 document.querySelectorAll('[data-mf-init]').forEach(form => {
   new AjaxForm({form: form});
 });
-
-// Convert container & response to accept "String" or "Function" in order do add default response function on one place
-
-// Example
-new AjaxForm({
-  form: '.mf-init',
-  container: '.mf-response-container',
-  // additionalData: {
-  //  lng: document.getElementById('language').value,
-  // },
-  queryParams: true,
-  pagination: {
-    // type: 'load-more',
-    type: 'pagination',
-    pattern: '<< < ~ >... [4] ...< ~ > >>',
-    // pattern: '< (x/y) >',
-    container: '.mf-pagination',
-    // pageSize: 10,
-    // totalCount: 90
-    pageCount: 9
-  },
-  paginationModel: { //pagination mapping
-    totalCount: 'Ukupno'
-  },
-  beforesend: () => {
-    // Add code
-  },
-  // response: function (res) {
-  //   console.log(res);
-  // }
-});
-
-//u slucaju da su podaci zavuceni u objekte, splituj po tacki i ispitaj kao sto je napisano dole
-// obj['key']['key2']
